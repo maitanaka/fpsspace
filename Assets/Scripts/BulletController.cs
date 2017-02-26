b8bd43b4c221bf42bcda79fdbca086b4b0c65a05
Mai Tanaka <bearbearbear9@gmail.com>
2017-02-26 22:50:51 +0900

    Create BulletController.cs to manage the number of bullets and bulletboxes.
    - the number of each can be controlled from inspector view
    - the number of the bullets decrease by 1 with the gun shot
    - script attached to the Player

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BulletController : MonoBehaviour {
+
+	[SerializeField] int bulletCount;
+	[SerializeField] int bulletBoxCount;
+
+
+	// Use this for initialization
+	void Start () {
+		bulletCount = 30;
+		bulletBoxCount = 150;
+	}
+
+	public void changeBulletCount(){
+		bulletCount -= 1;
+	}
+
+	public void ChangeBulletBoxCount(){
+		bulletBoxCount -= 1;
+	}
+}
