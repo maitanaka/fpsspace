fb993f1abaa0d6b7edab12471ac69c26b22abeb4
Mai Tanaka <bearbearbear9@gmail.com>
2017-02-26 22:48:09 +0900

    Create GunController.cs to be able to shoot a gun.
    - Instantiate bullet fire when shot
    - set ray from the gun
    - set interval between shots
    - set audio to play when shot

---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour {
+
+	[SerializeField] GameObject Bulletfire;
+	[SerializeField] GameObject player;
+
+	private Vector3 bulletHitPosition;
+	private AudioSource audioSource;
+	private AudioClip bulletFireSound;
+	private GameObject bulletFire;
+	private float bulletInterval;
+
+	void Start(){
+		bulletInterval = 0;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		bulletInterval += Time.deltaTime;
+		if (Input.GetMouseButton (0)) {
+			if (bulletInterval >= 0.2f) {
+				RaycastHit hit = new RaycastHit ();
+				if (Physics.Raycast (ray, out hit)) {
+					Shoot (hit);
+				}
+			}
+		}
+	}
+
+	private void Shoot(RaycastHit hit){
+		bulletInterval = 0.0f;
+		BulletController bulletController = player.GetComponent<BulletController> ();
+		bulletHitPosition = hit.point;
+		bulletFire = Instantiate (Bulletfire, bulletHitPosition, transform.rotation);
+		audioSource = bulletFire.GetComponent<AudioSource> ();
+		bulletFireSound = audioSource.GetComponent<AudioClip> ();
+		audioSource.PlayOneShot (bulletFireSound);
+		bulletController.changeBulletCount ();
+		Destroy (bulletFire, 0.1f);
+	}
+}
