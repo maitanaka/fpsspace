2f04b4b8bdb6b55bff9eb35359b59d69129654de
Mai Tanaka <bearbearbear9@gmail.com>
2017-03-02 15:53:10 +0900

    Set target’s hp and make it broken when hp reaches 5 - set animation. Control it using bool “broken”

---
+++ Assets/Scripts/TargetController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetController : MonoBehaviour {
+
+	public int hp;
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		hp = 5;
+		anim = GetComponent<Animator>();
+	}
+
+	void OnTriggerEnter(Collider coll){
+		if (coll.gameObject.tag == "PlayerBullet") {
+			if (hp == 0) {
+				anim.SetBool ("broken", true);
+			} else {
+				hp -= 1;
+			}
+		}
+	}
+
+}
