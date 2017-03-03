5f3ada856ecedf9d7d35ef8e435ce7895660ce83
Mai Tanaka <bearbearbear9@gmail.com>
2017-03-03 22:02:52 +0900

    Delete OnTriggerEnter method to use the script just to manage the animation and hp - not using onTriggerEnter but using ray from the gun

--- Assets/Scripts/TargetController.cs
+++ Assets/Scripts/TargetController.cs
@@ -5,22 +5,11 @@ using UnityEngine;
 public class TargetController : MonoBehaviour {
 
 	public int hp;
-	private Animator anim;
+	public Animator anim;
 
 	// Use this for initialization
 	void Start () {
 		hp = 5;
 		anim = GetComponent<Animator>();
 	}
-
-	void OnTriggerEnter(Collider coll){
-		if (coll.gameObject.tag == "PlayerBullet") {
-			if (hp == 0) {
-				anim.SetBool ("broken", true);
-			} else {
-				hp -= 1;
-			}
-		}
-	}
-
 }
