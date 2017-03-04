d88789a957ba2372637671491a8c9d9eae48fd79
Mai Tanaka <bearbearbear9@gmail.com>
2017-03-05 00:28:44 +0900

    Create PlayerController to attach to the FirstPersonCharacter and make it an origin point of the ray to shoot a gun; delete building ray part from GunController.cs

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	[SerializeField] GunController gunController;
+	[SerializeField] BulletController player;
+	[SerializeField] TargetController target;
+
+	public float bulletInterval;
+
+	// Use this for initialization
+	void Start(){
+		bulletInterval = 0;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Ray ray = new Ray(transform.position,transform.forward);
+		bulletInterval += Time.deltaTime;
+		if (Input.GetMouseButton (0)) {
+			if (bulletInterval >= 0.2f) {
+				RaycastHit hit = new RaycastHit ();
+				if (Physics.Raycast (ray, out hit)) {
+					if (player.bulletCount != 0) {
+						Vector3 raydirection = ray.direction;
+						gunController.Shoot (hit , raydirection);
+						if (hit.collider.gameObject.GetComponent<TargetController>() != null) {
+							target.ShootTarget (hit);
+						}
+					}
+				}
+			}
+		}
+
+		if(Input.GetKey(KeyCode.R)){
+			if (player.bulletCount < 30) {
+				gunController.Reload ();
+			}
+		}
+	}
+}
